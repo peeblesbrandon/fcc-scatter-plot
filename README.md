740dd588b4fa75f8588070537d121d0c86ec7c18
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-27 10:44:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FCC Scatter Plot
+
+Plotting bicycle racing times over time grouped by whether they involved doping allegations. Built according to freeCodeCamp specifications.
---
+++ src/script.js
@@ -0,0 +1,8 @@
+var w = 1000;
+var h = 500;
+var padding = 50;
+
+var svg = d3.select("#chart")
+            .append("svg")
+            .attr("height", h)
+            .attr("width", w)
