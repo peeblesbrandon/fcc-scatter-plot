ddd4c064bfff898302b9cd1b5e0de0bdf8d89424
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-27 16:51:53 -0400

    correct axes and add tooltips

--- src/script.js
+++ src/script.js
@@ -21,29 +21,40 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
     
     // var yearsArr = dataset.
     var parseYear  = d3.timeParse('%Y'); 
-    var formatYear = d3.timeFormat('%Y'); 
+    var formatYear = d3.timeFormat('%Y');
+    // var parseSecond = d3.timeParse('%S'); 
     var parseTime = d3.timeParse('%M:%S');
     var formatTime = d3.timeFormat('%M:%S');
 
+    var parsedYears = dataset.map((d) => {
+        return parseYear(d.Year);
+    });
+    var parsedTimes = dataset.map((d) => {
+        return parseTime(d.Time);
+    });
+
     // create scales
     const xScale = d3.scaleTime()
-        .domain([d3.min(dataset.map(d => parseYear(d.Year))), d3.max(dataset.map(d => parseYear(d.Year)))])
+        .domain(d3.extent(parsedYears))
         .range([padding, w - padding])
-    xScale.ticks(d3.timeYear.every(2));
-    const yScale = d3.scaleLinear()
-        .domain([d3.min(dataset.map(d => d.Seconds)), d3.max(dataset.map(d => d.Seconds))])
-        .range([h - padding, padding]);
-    // yScale.ticks(d3.timeMinute.every(1));
+        .nice();
+    const yScale = d3.scaleTime()
+        .domain(d3.extent(parsedTimes))
+        .range([h - padding, padding])
+        .nice();
 
     // AXES
     // add scales to axes
     const xAxis = d3.axisBottom()
-                    .scale(xScale);
+                    .scale(xScale)
+                    .ticks(d3.timeYear.every(2));
     const yAxis = d3.axisLeft()
                     .scale(yScale)
-                    .tickFormat((d, i) => {
-                        return dataset[i].Time;
-                    });
+                    // .tickValues(parsedTimes)
+                    .tickFormat((d) => {
+                        return formatTime(d);
+                    })
+                    .ticks(d3.timeSecond.every(30));
     // append group and insert axes
     svg.append("g")
         .call(xAxis)
@@ -60,9 +71,36 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
             .enter()
             .append("circle")
             .attr("class", "dot")
+            .attr("data-xvalue", (d) => parseYear(d.Year))
+            .attr("data-yvalue", (d) => parseTime(d.Time))
             .attr("cx", (d) => xScale(parseYear(d.Year)))
-            .attr("cy", (d) => yScale(d.Seconds))
-            .attr("r", "5");
+            .attr("cy", (d) => yScale(parseTime(d.Time)))
+            .attr("r", 7)
+            .style("fill", (d) => {
+                return d.Doping === "" ? "lightgreen" : "darkred";
+            })
+            .on("mouseover", function(d) {
+                d3.select(this).transition()
+                    .duration("200")
+                    .attr("r", 15);
+                tooltipDiv.transition()
+                    .duration("200")
+                    .style("opacity", 0.9)
+                tooltipDiv.html("<b>Name: </b>" + d.Name + " (" + d.Nationality + ")"
+                    + "</br><strong>Year: </b>" + d.Year + ", <b>Time: </b>" + d.Time
+                    + (d.Doping === "" ? "" : '</br><b>Summary: ' + d.Doping))
+                    .style("left", (d3.event.pageX + 25) + "px")
+                    .style("top", (d3.event.pageY - 20) + "px")
+                    .attr("data-year", parseYear(d.Year));
+            })
+            .on("mouseout", function(d) {
+                d3.select(this).transition()
+                    .duration("200")
+                    .attr("r", 7);
+                tooltipDiv.transition()
+                    .duration("200")
+                    .style("opacity", 0);
+            });
 
     
 });
